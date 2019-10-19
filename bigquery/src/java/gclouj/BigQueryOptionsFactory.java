74f46918e4e93e212116c51958688f388cb69caf
Gobinath Manokaran <>
2019-10-19 15:27:36 +0530

    Changing the big query builder

--- bigquery/src/java/gclouj/BigQueryOptionsFactory.java
+++ bigquery/src/java/gclouj/BigQueryOptionsFactory.java
@@ -3,9 +3,8 @@ package gclouj;
 import com.google.cloud.bigquery.BigQueryOptions;
 
 public class BigQueryOptionsFactory {
-  public static BigQueryOptions create(String projectId) {
-    BigQueryOptions.Builder builder = BigQueryOptions.builder()
-      .projectId(projectId);
+  public static BigQueryOptions create() {
+    BigQueryOptions.Builder builder = BigQueryOptions.newBuilder();
     return builder.build();
   }
 }
